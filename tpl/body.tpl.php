5239ca5f29b1e811f22af93b1bed10e4a5d9e63d
Andrew Ross <andrew.ross@eclipse.org>
2015-04-29 13:40:27 -0400

    add discount to the member discount pricing text

--- tpl/body.tpl.php
+++ tpl/body.tpl.php
@@ -129,7 +129,7 @@ if (!defined('ABSPATH')) exit;
         <h2>Sponsorship Prices</h2>
         <div class="price-badge clearfix">
           <div class="content-box col-sm-8"><h3>Sponsor the conference as a member</h3></div>
-          <div class="arrow_box col-sm-4"><span class="price">$2000</span>Members</div>
+          <div class="arrow_box col-sm-4"><span class="price">$2000</span>Member Discount</div>
         </div>
         <div class="price-badge clearfix price-badge-odd">
           <div class="content-box col-sm-8"><h3>Sponsor the conference as a non-member</h3></div>
