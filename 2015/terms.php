31ad29da837c045042a20dbfe71745d0eddf911f
Andrew Ross <andrew.ross@eclipse.org>
2015-05-27 10:52:46 -0400

    add initial terms of use file

---
+++ 2015/terms.php
@@ -0,0 +1,23 @@
+<?php
+/*******************************************************************************
+ * Copyright (c) 2015 Eclipse Foundation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andrew Ross(Eclipse Foundation) - Initial implementation
+ *******************************************************************************/
+
+
+require_once '../tpl/head.tpl.php';
+?>
+
+<p>Put TOU here</p>
+
+<?
+require_once '../tpl/footer.tpl.php';
+?>
+
+
