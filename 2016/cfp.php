e3ad28930738168d49fe875b8ae9e96fff6ed3d2
Andrea Ross <andrea.ross@eclipse.org>
2016-06-29 11:35:54 -0400

    Add initial placeholder for the CfP

---
+++ 2016/cfp.php
@@ -0,0 +1,29 @@
+<?php
+/*******************************************************************************
+ * Copyright (c) 2016 Eclipse Foundation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andrea Ross(Eclipse Foundation) - Initial implementation
+ *******************************************************************************/
+
+/** 
+ * Define ABSPATH as this files directory 
+ */
+define('ABSPATH', dirname(__FILE__) . '/2016/');
+
+require_once '../tpl/head.tpl.php';
+require_once '../tpl/top.tpl.php';
+?>
+
+
+<h1>Call For Proposals</h1>
+
+<p>TBD</p>
+
+<?
+require_once '../tpl/footer.tpl.php';
+?>
