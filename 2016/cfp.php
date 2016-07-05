df27cad28cc07814e736053d0f9a7f54d1c0fe62
Andrea Ross <andrea.ross@eclipse.org>
2016-07-05 10:02:06 -0400

    Submit 2016 CfP text.

--- 2016/cfp.php
+++ 2016/cfp.php
@@ -22,7 +22,40 @@ require_once '../tpl/top.tpl.php';
 
 <h1>Call For Proposals</h1>
 
-<p>TBD</p>
+<h2>Format</h2>
+
+<p>The conference consists of one day of sessions across 3 session rooms. Standard talks are 25 minutes long. Short talks are 15 minutes long. Lightning talks are 5 minutes long.</p>
+
+<h2>Conference Topics</h2>
+
+<p>Below is a list of topics that we think attendees would be excited to hear about for session presentations. The list is certainly not exhaustive and other interesting topics will be considered under the "Other Cool Stuff" category. In general, use cases will be preferred over pure technology talks. Emerging technology &amp; technique talks are very welcome.</p>
+
+<p>This conference heavily emphasizes open, however noteworthy talks on hybrid solutions using both open &amp; proprietary will be considered.</p>
+
+<h3>Open Source and Open Data policy and practice</h3>
+
+<p>Licenses, privacy, ethics, practicalities of open source/ open standards/ and open data use in government, success/failure stories, funding (seeking/offering), getting support, and migrating from legacy systems.</p>
+
+<h3>Processing data at scale</h3>
+
+<p>Remote sensing, feature extraction, machine learning, distributed computing (AWS, C2S/GovCloud).</p>
+
+<h3>Data Capture</h3>
+
+<p>Drones, kites, balloons, satellites, sensors, and other data capture channels.</p>
+
+<h3>Data visualization</h3>
+
+<p>Web mapping, 3D, Mobile, animating data changes</p>
+
+<h3>Citizen Science Enablement</h3>
+
+<p>Crowdsourcing and public participation, open collaboration, DIY, and related.</p>
+
+<h3>Other cool stuff</h3>
+
+<p>Anything you think is very interesting and worth talking about.</p>
+
 
 <?
 require_once '../tpl/footer.tpl.php';
