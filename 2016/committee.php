65c8c897d3f7d2323c08f25974912a3a1e08c595
Andrea Ross <andrea.ross@eclipse.org>
2016-07-05 12:57:06 -0400

    Placeholder for committee page

---
+++ 2016/committee.php
@@ -0,0 +1,32 @@
+<?php
+/*******************************************************************************
+ * Copyright (c) 2016 Eclipse Foundation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andrea Ross(Eclipse Foundation) - Initial implementation
+ *******************************************************************************/
+
+/** 
+ * Define ABSPATH as this files directory 
+ */
+define('ABSPATH', dirname(__FILE__) . '/2016/');
+
+require_once '../tpl/head.tpl.php';
+require_once '../tpl/top.tpl.php';
+?>
+
+
+<h1>Our Committee</h1>
+
+<p>The conference would not happen without the efforts of our wonderful team. Please help us thank them!</p>
+
+<p>Table with bios and pictures TBD</p>
+
+
+<?
+require_once '../tpl/footer.tpl.php';
+?>
