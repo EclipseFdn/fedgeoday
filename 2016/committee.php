18559a8269850c4999510f54e7761b935103875e
Andrea Ross <andrea.ross@eclipse.org>
2016-09-02 10:26:41 -0400

    Try a bit of a buffer around the table cells for 2016 committee page

--- 2016/committee.php
+++ 2016/committee.php
@@ -27,53 +27,53 @@ require_once '../tpl/top.tpl.php';
 
 <table style="width:100%">
   <tr style="vertical-align: top;">
-    <td style="vertical-align: top;width: 33%;" >
+    <td style="vertical-align: top;width: 33%; margin: 20px;" >
       <image style="margin: 10px; float: left;" src="../images/committee/Cozzi.jpg">
       <p><strong>Patrick Cozzi</strong></p>
       <p>Patrick Cozzi is our program chair for 2016 and led the team for program selection.
       <p>Patrick started Cesium and focuses on the graphics engine, 3D formats, and community outreach.
 As the Cesium lead at AGI, Patrick contributes to all things Cesium, including roadmap planning, software development, recruitment, outreach, and business development. Patrick loves 3D graphics and is the editor of WebGL Insights, coeditor of OpenGL Insights, coauthor of 3D Engine Design for Virtual Globes, and a member of the editorial board for the Journal of Computer Graphics Techniques. He is co-creator of glTF and started 3D Tiles. Patrick is active in the graphics and open-source communities, develops open-standards with Khronos, frequently presents at conferences, and is on the organizing committee for FOSS4G NA 2016. He also teaches GPU Programming and Architecture at the University of Pennsylvania, where he earned a master’s degree in computer science.</p>
     </td>
-    <td style="vertical-align: top;width: 33%;" >
+    <td style="vertical-align: top;width: 33%; margin: 20px;" >
       <image style="margin: 10px; float: left;" src="../images/committee/Lyon.jpg">
       <p><strong>Elizabeth Lyon</strong></p>
       <p>Elizabeth Lyon is an innovator and researcher active in the geospatial, social science and open-source communities. In Washington, DC she leads several community initiatives including the GeoDC community, the Geo-Northern Virginia Community, the MapStory community, and Women in GIS. She also volunteers internationally building geospatial solutions and supporting community development. Liz is currently  completing her PhD in Geospatial Sciences at George Mason University, where her research focuses on virtual and physical environments generation narrative that shape global urban forms and functions. She earned a Post-Graduate Certificate in Computational Social Science from George Mason University, a Master of Science in Geography from University of Illinois, Urbana-Champaign and a Bachelor of Arts in Economics and French from Augustana College (Rock Island, IL).</p>
     </td>
-    <td style="vertical-align: top;width: 33%;" >
+    <td style="vertical-align: top;width: 33%; margin: 20px;" >
       <image style="margin: 10px; float: left;" src="../images/committee/Kunce.jpg">
       <p><strong>Dale Kunce</strong></p>
       <p>Dale Kunce leads the international GIS team at the <a href="http://redcross.org/">American Red Cross</a> where he is responsible for the adoption and creation of open source software such as OpenMapKit, open data standards and OpenStreetMap to supercharge the work of the American Red Cross. Dale is a geospatial engineer and urban planner with more than ten years of experience working directly with communities throughout the world. He and his team support programs and projects for both international disaster response and long-term programs. Kunce’s past work has taken him from community organizing in the California Central Valley and developing analytics for pandemic response in Uganda to helping astronauts capture high-resolution imagery from the International Space Station. He has helped coordinate the Red Cross response efforts for the last couple of years, working closely with HOT. He is a founder of <a href="http://missingmaps.org/">Missing Maps</a> and a frequent contributor OSM under the username <a href="http://www.openstreetmap.org/user/dkunce">dkunce</a>. Dale is a current board member, elected during the June 2015 Special Election.</p>
     </td>
   </tr>
   <tr style="vertical-align: top;">
-    <td style="vertical-align: top;width: 33%;" >
+    <td style="vertical-align: top;width: 33%; margin: 20px;" >
       <image style="margin: 10px; float: left;" src="../images/committee/Levine.jpg">
       <p><strong>Rachel Levine</strong></p>
       <p>As a Program Associate with the GIS team, Rachel handles operational needs and keeps the team on track and in line. Before joining the Red Cross, she completed a master’s degree in Activism and Social Change at the University of Leeds’ geography department and handled operations at other non-profit organizations. She is also a UMaryland alumna and a huge fan of parks. When not attending mapathons, Rachel likes to travel and wander new cities.</p>
     </td>
-    <td style="vertical-align: top;width: 33%;" >
+    <td style="vertical-align: top;width: 33%; margin: 20px;" >
       <image style="margin: 10px; float: left;" src="../images/committee/Tucker.jpg">
       <p><strong>Christopher Tucker</strong></p>
       <p>Dr. Christopher Tucker manages Yale House Ventures, a portfolio of social ventures and technology companies that span the worlds of energy, geospatial, sensor, cyber-security, open source, and social media technologies, across the domains of defense/intelligence, international affairs, civilian government, commercial industry, NGOs, and academe. He is also the Chairman and CEO of The MapStory Foundation and President of the foreign policy advocacy group, Friends of the Arc. Dr. Tucker was previously Senior VP for the Americas/National Programs at ERDAS and President/CEO of IONIC, a leader in interoperable web-mapping, location based services, imagery management and distributed geoprocessing. He has served on a variety of Boards such as the US Geospatial intelligence Foundation, the Open Geospatial Consortium, Open Plans, OpenGeo, the Secretary of Interior’s National Geospatial Advisory Committee, and the Defense Science Board Intelligence Task Force, the DNI’s Intelligence Community Strategic Studies Group, and serves as an Independent Advisor to the Director of the National Geospatial Intelligence Agency (NGA). Dr. Tucker served on the National Academy of Science’s Committee on NGA’s GEOINT Research Priorities.</p>
     </td>
-    <td style="vertical-align: top;width: 33%;" >
+    <td style="vertical-align: top;width: 33%; margin: 20px;" >
       <image style="margin: 10px; float: left;" src="../images/committee/Sheets.jpg">
       <p><strong>Kari Sheets</strong></p>
       <p>bio goes here</p>
     </td>
   </tr>
   <tr style="vertical-align: top;">
-    <td style="vertical-align: top;width: 33%;" >
+    <td style="vertical-align: top;width: 33%; margin: 20px;" >
       <image style="margin: 10px; float: left;" src="../images/committee/Pickle.jpg">
       <p><strong>Eddie Pickle</strong></p>
       <p>Eddie Pickle is Managing Director Open Source Programs at RadiantBlue Technologies. Previously he was the founding CEO of Boundless, and led its 2013 spinout from non-profit to private company. A human geographer by training, he has been a leader in both the geodemographics revolution of the 1980’s and 1990’s, and the geospatial web services explosion of the past two decades. He has served as a site location specialist, market segmentation analyst, sales and consulting leader, critical infrastructure visualization expert, and senior executive, and has worked at both leading international software and data companies. Eddie is a founding Steering Committee of the Eclipse Foundation’s LocationTech industry working group. Previous to his service at Boundless, Eddie was one of the founders of geo web services pioneer IONIC Enterprise and served as its Chief Operating Officer until its purchase by ERDAS in 2007.</p>
     </td>
-    <td style="vertical-align: top;width: 33%;" >
+    <td style="vertical-align: top;width: 33%; margin: 20px;" >
       <image style="margin: 10px; float: left;" src="../images/committee/Callaway.jpg">
       <p><strong>Jason Callaway</strong></p>
       <p>Jason Callaway is a senior solutions architect at Red Hat, specializing in secure cloud computing. In his current role, Jason is the technical lead working across the US Government agencies to help implement their cloud migration strategies. Prior to joining Red Hat, Jason deployed the first mission OpenShift Platform as a Service environment in the Department of Defense. As a result of his work with PaaS frameworks, Jason has been focusing on the containerization of geospatial services in the interest of lowering the user and developer barriers to entry, see <a href="http://geopaas.io">http://geopaas.io</a> for details. Jason speaks frequently at open source and cryptography meetups, his blog is at <a href="https://blog.jasoncallaway.com">https://blog.jasoncallaway.com</a>.</p>
     </td>
-    <td style="vertical-align: top;width: 33%;" >
+    <td style="vertical-align: top;width: 33%; margin: 20px;" >
       <image style="margin: 10px; float: left;" src="../images/committee/Calamito.jpg">
       <p><strong>Anthony Calamito</strong></p>
       <p>bio goes here</p>
